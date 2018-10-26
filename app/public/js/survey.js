7c07ca92964fbf891d49c278d5a849811ba077c4
DeOnna <36292560+DeOnna05@users.noreply.github.com>
2018-10-26 12:01:03 -0500

    validation added

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 
 })
 
-
 $('#submit').on('click', function () {
     let surveyForm = {
         name: $("#first_name").val().trim(),
@@ -21,17 +20,44 @@ $('#submit').on('click', function () {
             $('#q10').val(),
         ]
     }
-    $.ajax({
-        url: '/api/employees',
-        method: 'POST',
-        data: surveyForm
-    }).then(function (response) {
-        //putting the link to the picture and name in variables
-        let picture = response.photo;
-        let name = response.name;
-        $('.modal').modal('open');
-        $('#picture').attr('src', picture);
-        $('.matchName').text(name);
-    })
-    
-});
+    console.log(surveyForm)
+    let pictureInput = surveyForm.photo;
+    let nameInput = surveyForm.name;
+    let scoreInput = surveyForm.scores;
+    //loop through scores to find any that are null
+    for (let i = 0; i < scoreInput.length; i++) {
+        //if any inputs are empty modal will tell user to fill out all fields
+        if (pictureInput === "" || nameInput === "" || scoreInput[i] === null) {
+            console.log('Please fill out all fields');
+            
+            return $('#modal2').modal('open') ;
+        }
+    } {
+        //if all fields are completed, call will grab form info and run post function
+        $.ajax({
+            url: '/api/employees',
+            method: 'POST',
+            data: surveyForm
+        }).then(function (response) {
+            //putting the link to the picture and name in variables
+            let picture = response.photo;
+            let name = response.name;
+            $('.modal').modal('open');
+            $('#picture').attr('src', picture);
+            $('.matchName').text(name);
+            $("#first_name").val('');
+            $("#link_image").val('');
+            $('#q1').val('');
+            $('#q2').val('');
+            $('#q3').val('');
+            $('#q4').val('');
+            $('#q5').val('');
+            $('#q6').val('');
+            $('#q7').val('');
+            $('#q8').val('');
+            $('#q9').val('');
+            $('#q10').val('');
+
+        })
+    }
+})
